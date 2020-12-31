bd8642c7db7e7b7b6235fc04dd358d4c30c29011
Chrish Dunne <hey@chrish.design>
2020-12-31 12:58:00 +0000

    chore: Added plugins
    Added:
    - Sanity source
    - Styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,1 +1,28 @@
+import dotenv from 'dotenv';
+
 //
+
+dotenv.config({ path: '.env' });
+
+//
+
+export default {
+  siteMetadata: {
+    title: 'DSCVRD Magazine',
+    siteUrl: 'https://www.discoveredmagazine.com/',
+    description:
+      'International Music & Entertainment magazine bringing you the best from UK, Europe & America',
+  },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'lylk5ufs',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
+};
