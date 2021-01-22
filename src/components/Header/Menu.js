bb8cf37fc21dd00f66d36230fc1ee6c014929db6
Chrish Dunne <hey@chrish.design>
2021-01-22 12:35:12 +0000

    style: Added non-responsive menu style

--- src/components/Header/Menu.js
+++ src/components/Header/Menu.js
@@ -2,27 +2,63 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Helmet } from 'react-helmet';
+import { Link } from 'gatsby';
 
 //
 
 const Menu = ({ status }) => {
-  console.log(status);
-
   if (status) {
     return (
       <>
         <Helmet>
           <style type="text/css">
             {`
-          body {
-            overflow: hidden;
-          }
-        `}
+              body {
+                overflow: hidden;
+              }
+            `}
           </style>
         </Helmet>
 
-        <MenuWrapper>
-          <h1>Soon 😎</h1>
+        <MenuWrapper theme="white">
+          <ul>
+            <li>
+              <Link to="/">
+                <h2>Drop</h2>
+                <p>All things from the fashion aspect</p>
+              </Link>
+            </li>
+            <li>
+              <Link to="/">
+                <h2>Kulture</h2>
+                <p>All things from the fashion aspect</p>
+              </Link>
+            </li>
+            <li>
+              <Link to="/">
+                <h2>Format</h2>
+                <p>All things from the fashion aspect</p>
+              </Link>
+            </li>
+            <li>
+              <Link to="/">
+                <h2>Noise</h2>
+                <p>All things from the fashion aspect</p>
+              </Link>
+            </li>
+            <li>
+              <Link to="/">
+                <h2>Shred</h2>
+                <p>All things from the fashion aspect</p>
+              </Link>
+            </li>
+            <li>
+              <Link to="/">
+                <h2>Contact</h2>
+                <p>All things from the fashion aspect</p>
+              </Link>
+            </li>
+          </ul>
         </MenuWrapper>
       </>
     );
@@ -41,6 +77,46 @@ const MenuWrapper = styled.nav`
   height: 100vh;
   background-color: var(--yellow);
   place-items: center;
+
+  ul {
+    display: grid;
+    grid-column-gap: 10rem;
+    grid-row-gap: 2.4rem;
+    grid-template-columns: 1fr 1fr;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  h2 {
+    color: var(--${(props) => props.theme});
+    font-size: 10.4rem;
+    font-weight: bold;
+    line-height: 11.4rem;
+    text-transform: uppercase;
+
+    @supports (-webkit-text-stroke: 1px var(--${(props) => props.theme})) {
+      color: transparent;
+      -webkit-text-stroke: 1px var(--${(props) => props.theme});
+    }
+  }
+
+  a {
+    text-decoration: none;
+
+    @supports (-webkit-text-stroke: 1px var(--${(props) => props.theme})) {
+      &:hover h2 {
+        color: var(--${(props) => props.theme});
+      }
+    }
+  }
+
+  p {
+    color: var(--${(props) => props.theme});
+    font-family: var(--font-serif-copy);
+    font-size: 1.6rem;
+    line-height: 2.4rem;
+  }
 `;
 
 Menu.propTypes = {
