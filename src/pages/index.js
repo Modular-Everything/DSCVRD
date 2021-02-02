0800254996311257604d3d17c5c9467fb60552ea
Chrish Dunne <hey@chrish.design>
2021-02-02 20:29:02 +0000

    feat: Added countdown

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,47 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { graphql } from 'gatsby';
 import styled from 'styled-components';
 import { useForm } from 'react-hook-form';
+import Countdown from 'react-countdown';
 import Logo from '../images/logo__white.png';
+import BG from '../images/bg.jpg';
 import Layout from '../components/Layout';
+
+//
+
+const Completionist = () => <span>You are good to go!</span>;
+
+const renderer = ({ days, hours, minutes, seconds, completed }) => {
+  if (completed) {
+    // Render a completed state
+    return <Completionist />;
+  }
+  // Render a countdown
+  return (
+    <ul>
+      <li>
+        <span>{days}</span> Day{days !== 1 && 's'}
+      </li>
+      <li>
+        <span>:</span>
+      </li>
+      <li>
+        <span>{hours}</span> Hr{hours !== 1 && 's'}
+      </li>
+      <li>
+        <span>:</span>
+      </li>
+      <li>
+        <span>{minutes}</span> Min{minutes !== 1 && 's'}
+      </li>
+      <li>
+        <span>:</span>
+      </li>
+      <li>
+        <span>{seconds}</span> Sec{seconds !== 1 && 's'}
+      </li>
+    </ul>
+  );
+};
 
 //
 
@@ -20,6 +57,9 @@ const HomePage = () => {
         <div className="layout__content">
           <div className="layout__content--left">
             <img src={Logo} alt="" />
+            <div className="countdown">
+              <Countdown date={Date.now() + 50000000000} renderer={renderer} />
+            </div>
           </div>
 
           <div className="layout__content--right">
@@ -56,31 +96,72 @@ const HomePage = () => {
   );
 };
 
-HomePage.propTypes = {};
-
 export default HomePage;
 
 const Content = styled.section`
   display: grid;
-  width: 100vw;
-  height: 100vh;
+  width: calc(100vw - 6.4rem);
+  height: calc(100vh - 6.4rem);
+  padding: 3.2rem;
+  background: url(${BG}) no-repeat;
+  background-size: cover;
   place-content: center;
 
   .layout__content {
-    display: flex;
+    display: grid;
+    grid-gap: 4.8rem;
+    grid-template-columns: 1fr;
     padding: 3.2rem;
     background-color: var(--black);
+
+    @media (min-width: 900px) {
+      grid-gap: 2.4rem;
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+
+  .countdown {
+    max-width: 32rem;
+    color: var(--white);
+    font-family: var(--font-sans);
+    font-weight: bold;
+    text-align: center;
+    text-transform: uppercase;
+
+    ul {
+      display: grid;
+      grid-template-columns: repeat(7, 1fr);
+      margin: 0;
+      padding: 0;
+      list-style: none;
+
+      li {
+        font-size: 1.6rem;
+      }
+
+      span {
+        color: var(--yellow);
+        font-size: 3.8rem;
+        line-height: 5rem;
+      }
+    }
   }
 
   h1 {
-    margin-bottom: 2.4rem;
+    max-width: 62rem;
+    margin-bottom: 3.2rem;
     color: var(--white);
     font-family: var(--font-sans);
-    font-size: 3.8rem;
+    font-size: 3.2rem;
     font-weight: bold;
     line-height: 4rem;
     text-align: center;
     text-transform: uppercase;
+
+    @media (min-width: 666px) {
+      font-size: 3.8rem;
+      line-height: 4rem;
+    }
   }
 
   small {
@@ -99,7 +180,7 @@ const Content = styled.section`
     max-width: 32rem;
 
     .error {
-      margin: 0 0 1.6rem;
+      margin: 1.6rem 0;
       color: var(--yellow);
       font-size: 1.4rem;
       text-align: center;
@@ -107,7 +188,7 @@ const Content = styled.section`
 
     input {
       width: calc(100% - 2.4rem);
-      margin: 0 0 1.6rem 0;
+      margin: 0;
       padding: 1.2rem;
       border: 1px solid #888;
       background-color: var(--black);
@@ -145,9 +226,6 @@ const Content = styled.section`
   .layout__content--left,
   .layout__content--right {
     display: grid;
-    width: 50%;
     place-items: center;
   }
 `;
-
-// export const query = graphql``;
