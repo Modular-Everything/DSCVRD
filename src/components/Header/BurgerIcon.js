bc820a5b9295ff5399c1e4e8be4139ffb4525bc5
Chrish Dunne <hey@chrish.design>
2021-02-27 10:38:23 +0000

    feat: Added close animation

--- src/components/Header/BurgerIcon.js
+++ src/components/Header/BurgerIcon.js
@@ -4,33 +4,57 @@ import styled from 'styled-components';
 
 //
 
-const BurgerIcon = ({ callback, status, invert }) => (
-  <Button type="button" onClick={callback} status={status} invert={invert}>
-    <span />
-    <span />
-    <span />
-  </Button>
-);
+const BurgerIcon = ({ callback, status, invert }) => {
+  console.log(status);
+
+  return (
+    <Button type="button" onClick={callback} status={status} invert={invert}>
+      <span />
+      <span />
+      <span />
+    </Button>
+  );
+};
 
 export default BurgerIcon;
 
 const Button = styled.button`
-  display: grid;
-  grid-gap: 0.7rem;
-  grid-template-rows: 1fr 1fr 1fr;
-  width: 4rem;
-  height: 2.5rem;
+  position: relative;
+  width: 3.2rem;
+  height: 2.4rem;
+  margin: 0;
+  padding: 0;
   border: 0;
   outline: none;
   background-color: transparent;
   cursor: pointer;
 
   span {
+    position: absolute;
+    left: 0;
     width: 100%;
     height: 2px;
+    transform-origin: center;
+    transition: 150ms ease all;
     background-color: var(
       --${(props) => (!props.status || props.invert === 'dark' ? 'white' : 'black')}
     );
+
+    &:nth-of-type(1) {
+      top: ${(props) => (props.status ? 'calc(50% - 2px);' : 0)};
+      transform: rotate(${(props) => (props.status ? '-45deg' : 0)});
+    }
+
+    &:nth-of-type(2) {
+      top: calc(50% - 1px);
+      transform: rotate(0);
+      opacity: ${(props) => (props.status ? 0 : 1)};
+    }
+
+    &:nth-of-type(3) {
+      bottom: ${(props) => (props.status ? '50%' : 0)};
+      transform: rotate(${(props) => (props.status ? '45deg' : 0)});
+    }
   }
 `;
 
