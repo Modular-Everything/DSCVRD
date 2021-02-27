797a9df897cf65259bf51d0e6541fcee80cb6f51
Chrish Dunne <hey@chrish.design>
2021-02-27 17:26:06 +0000

    feat: Added some quick win plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,7 @@ export default {
   },
 
   plugins: [
+    `gatsby-plugin-sitemap`,
     'gatsby-plugin-styled-components',
     {
       resolve: `gatsby-plugin-manifest`,
@@ -32,6 +33,7 @@ export default {
         icon: `./src/images/favicon.png`,
       },
     },
+    `gatsby-plugin-offline`, // important - keep this below the manifest
     {
       resolve: 'gatsby-source-sanity',
       options: {
@@ -50,5 +52,39 @@ export default {
         timeout: 3500,
       },
     },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `#ffef00`,
+        showSpinner: false,
+      },
+    },
+    // {
+    //   resolve: `gatsby-plugin-google-gtag`,
+    //   options: {
+    //     // You can add multiple tracking ids and a pageview event will be fired for all of them.
+    //     trackingIds: [
+    //       "GA-TRACKING_ID", // Google Analytics / GA
+    //       "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+    //       "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+    //     ],
+    //     // This object gets passed directly to the gtag config command
+    //     // This config will be shared across all trackingIds
+    //     gtagConfig: {
+    //       optimize_id: "OPT_CONTAINER_ID",
+    //       anonymize_ip: true,
+    //       cookie_expires: 0,
+    //     },
+    //     // This object is used for configuration specific to this plugin
+    //     pluginConfig: {
+    //       // Puts tracking script in the head instead of the body
+    //       head: false,
+    //       // Setting this parameter is also optional
+    //       respectDNT: true,
+    //       // Avoids sending pageview hits from custom paths
+    //       exclude: ["/preview/**", "/do-not-track/me/too/"],
+    //     },
+    //   },
+    // },
   ],
 };
