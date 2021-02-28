d7756771aa37fb174f03b0eea583823b0b1d9df9
Chrish Dunne <hey@chrish.design>
2021-02-28 19:29:21 +0000

    fix: Styling issue

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Layout from './src/components/Layout';
+
+export function replaceHydrateFunction() {
+  return (element, container, callback) => {
+    ReactDOM.render(element, container, callback);
+  };
+}
 
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>;
