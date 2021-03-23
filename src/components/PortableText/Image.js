4339f8319b929bcc714689c6fbc68310c3b26885
Chrish Dunne <hey@chrish.design>
2021-03-23 12:09:48 +0000

    feat: Pull in article images

--- src/components/PortableText/Image.js
+++ src/components/PortableText/Image.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import Img from 'gatsby-image';
-import { getFluidGatsbyImage } from 'gatsby-source-sanity';
-import clientConfig from '../../../client-config';
+import Img from 'react-cool-img';
 
 //
 
-const Image = ({ node }) => {
+const Image = (props) => {
+  const { node } = props;
   if (!node || !node.asset || !node.asset._ref) {
     return null;
   }
 
-  const fluidProps = getFluidGatsbyImage(
-    node.asset._ref,
-    { maxWidth: 1280, maxHeight: 800 },
-    clientConfig.sanity
-  );
+  let filename = node.asset._ref.replace('image-', '');
+  filename = filename.replace(/-([^-]*)$/, '.$1');
+  // eslint-disable-next-line react/destructuring-assignment
+  const imgUrl = `https://cdn.sanity.io/images/${props.options.projectId}/production/${filename}`;
 
   return (
     <ImgWrap data-sal="fade">
-      <Img fluid={fluidProps} alt={node.alt} />
+      <Img
+        placeholder={`//images.weserv.nl/?url=${imgUrl}?w=10&h=10&blur=5`}
+        src={`//images.weserv.nl/?url=${imgUrl}?w=1280&h=1024`}
+        alt=""
+      />
     </ImgWrap>
   );
 };
@@ -28,9 +30,14 @@ const Image = ({ node }) => {
 export default Image;
 
 const ImgWrap = styled.figure`
-  margin: 0;
+  margin: 0 0 4rem;
+
+  img {
+    width: 100%;
+  }
 `;
 
 Image.propTypes = {
   node: PropTypes.object.isRequired,
+  options: PropTypes.object.isRequired,
 };
