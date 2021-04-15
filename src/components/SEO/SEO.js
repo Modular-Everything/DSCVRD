140a579c6b3add42e7328a7f8452a28fc628379b
Chrish Dunne <hey@chrish.design>
2021-04-15 10:39:01 +0100

    feat: Further OG work

--- src/components/SEO/SEO.js
+++ src/components/SEO/SEO.js
@@ -1,79 +1,145 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
-import { useLocation } from '@reach/router';
 import { useStaticQuery, graphql } from 'gatsby';
+import { useLocation } from '@reach/router';
 
 import Card from '../../images/social-card.jpeg';
 
 //
 
-const SEO = ({ title, description, article, ogImage, ogTag }) => {
-  const query = graphql`
-    query SEO {
-      site {
-        siteMetadata {
-          defaultTitle: title
-          defaultDescription: description
-          siteUrl
-          titleTemplate
+const SEO = ({
+  article,
+  description,
+  lang,
+  meta,
+  ogImage,
+  ogTag,
+  title,
+  pathname,
+}) => {
+  const { pathname: defaultPath } = useLocation();
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            siteUrl
+            titleTemplate
+          }
         }
       }
-    }
-  `;
-
-  const { pathname } = useLocation();
-  const { site } = useStaticQuery(query);
-
-  const {
-    defaultTitle,
-    titleTemplate,
-    defaultDescription,
-    siteUrl,
-  } = site.siteMetadata;
-
-  const seo = {
-    title: title || defaultTitle,
-    description: description || defaultDescription,
-    url: `${siteUrl}${pathname}`,
-    image: ogImage || Card,
-    articleTag: ogTag,
-  };
+    `
+  );
+  const metaDescription = description || site.siteMetadata.description;
+  const image = ogImage || Card;
+  const canonical = pathname
+    ? `${site.siteMetadata.siteUrl}${pathname}`
+    : defaultPath;
 
   return (
     <Helmet
-      title={seo.title}
-      titleTemplate={titleTemplate}
       htmlAttributes={{
-        lang: 'en',
+        lang,
       }}
-    >
-      <meta name="description" content={seo.description} />
-      <meta name="image" content={seo.image} />
-      <meta property="og:title" content={seo.title} />
-      <meta property="og:url" content={seo.url} />
-      <meta name="og:image" content={seo.image} />
-      <meta
-        property="og:type"
-        content={article ? `article:${ogTag}` : `website`}
-      />
-    </Helmet>
+      title={title}
+      titleTemplate={site.siteMetadata.titleTemplate}
+      link={
+        canonical
+          ? [
+              {
+                rel: 'canonical',
+                href: canonical,
+              },
+            ]
+          : []
+      }
+      meta={[
+        {
+          name: `description`,
+          content: metaDescription,
+        },
+        // {
+        //   name: 'keywords',
+        //   content: site.siteMetadata.keywords.join(','),
+        // },
+        {
+          property: `og:title`,
+          content: title,
+        },
+        {
+          property: `og:description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:type`,
+          content: article ? `article:${ogTag}` : `website`,
+        },
+        // {
+        //   name: `twitter:creator`,
+        //   content: site.siteMetadata.author,
+        // },
+        {
+          name: `twitter:title`,
+          content: title,
+        },
+        {
+          name: `twitter:description`,
+          content: metaDescription,
+        },
+      ]
+        .concat(
+          ogImage
+            ? [
+                {
+                  property: 'og:image',
+                  content: image,
+                },
+                {
+                  property: 'og:image:width',
+                  content: ogImage.width,
+                },
+                {
+                  property: 'og:image:height',
+                  content: ogImage.height,
+                },
+                {
+                  name: 'twitter:card',
+                  content: 'summary_large_image',
+                },
+              ]
+            : [
+                {
+                  name: 'twitter:card',
+                  content: 'summary',
+                },
+              ]
+        )
+        .concat(meta)}
+    />
   );
 };
 
 export default SEO;
 
-SEO.propTypes = {
-  title: PropTypes.string,
-  description: PropTypes.string,
-  article: PropTypes.bool,
-  ogImage: PropTypes.string,
-  ogTag: PropTypes.string,
-};
 SEO.defaultProps = {
-  title: null,
-  description: null,
-  article: false,
+  lang: `en`,
+  meta: [],
+  description: ``,
   ogImage: null,
   ogTag: null,
+  article: false,
+};
+
+SEO.propTypes = {
+  article: PropTypes.bool,
+  description: PropTypes.string,
+  lang: PropTypes.string,
+  meta: PropTypes.arrayOf(PropTypes.object),
+  title: PropTypes.string.isRequired,
+  ogImage: PropTypes.string,
+  ogTag: PropTypes.string,
+  pathname: PropTypes.string,
 };
